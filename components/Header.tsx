4103d42814e77212c465afd173bf4d777598d50a
--global <sajjad.eros@gmail.com>
2022-11-19 16:41:45 +0330

    Initial Commit and project start

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>test 1</li>
+          <li>test 1</li>
+          <li>test 1</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
